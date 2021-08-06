fc4b72f52e9c0cfd2cb1da9cd9d04444ca970a23
Dominick Vale <dominickveil@gmail.com>
2021-08-06 15:04:08 +0200

    Add missing node installation

--- darbs.sh
+++ darbs.sh
@@ -146,9 +146,10 @@ installnvm(){
 	sudo -u "$name" git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
 	cd "$NVM_DIR"
 	sudo -u "$name" git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` && \. "$NVM_DIR/nvm.sh"
-
 	grep -qF '# This loads nvm' /home/$name/.config/zsh/.zshrc || echo 'export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> /home/$name/.config/zsh/.zshrc
+	\. /nvm.sh
+	nvm install latest
 }
 
 installohmyzsh(){
