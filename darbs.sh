188a1e997361a27876f1480111e9ac2443eaab8a
DominickVale <dominickveil@gmail.com>
2022-10-23 11:33:47 +0200

    update links

--- darbs.sh
+++ darbs.sh
@@ -6,8 +6,8 @@
 
 ### OPTIONS AND VARIABLES ###
 
-dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
-progsfile="https://raw.githubusercontent.com/LukeSmithxyz/LARBS/master/progs.csv"
+dotfilesrepo="https://github.com/DominickVale/voidrice.git"
+progsfile="https://raw.githubusercontent.com/DominickVale/DARBS/xorg/progs.csv"
 aurhelper="yay"
 repobranch="master"
 
